790e64a5e6235d042fb692aaea49f950b15a6277
Nico <nicolas.limonoff@gmail.com>
2023-05-07 11:13:09 -0300

    added more types

--- lesson-1/Types.sol
+++ lesson-1/Types.sol
@@ -8,4 +8,16 @@ contract Types {
     int256 favoriteInt = -5;
     address myAddress = 0xc9f200abB8a628af2ad821755F0119dAed0F5513;
     bytes32 favoriteBytes = "cat"; // Bytes are same as string
+
+
+    struct Student {
+        string name;
+        uint256 age;
+    }
+
+    // Arrays
+    Student[] public student;
+
+    // Dictionary
+    mapping(string => uint256) public ageOfStudent;
 }
